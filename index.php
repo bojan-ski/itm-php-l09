9e5abcc29d1fcc061927196fa2db780260fbea8c
Bojan SKI <codeucenje@gmail.com>
2025-01-10 09:24:12 +0100

    task done

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
-    
-?>
+
+function calculateTax($num)
+{
+    if (gettype($num) == 'string') die('Please provide a number');
+
+    if ($num <= 1) die('The number needs to higher than 1');
+
+    $result = $num * (22 / 100);
+
+    return $result;
+};
+
+$taxAmountToPay = calculateTax(100);
+echo ("You have to pay $$taxAmountToPay tax money");
+
+/*
+function calculateTax(int $num): float
+{
+    if (gettype($num) == 'string') die('Please provide a number');
+
+    if ($num <= 1) die('The number needs to higher than 1');
+
+    (float) $result = $num * (22 / 100);
+
+    return $result;
+};
+
+$taxAmountToPay = calculateTax(100);
+echo ("You have to pay $$taxAmountToPay tax money");
+*/
